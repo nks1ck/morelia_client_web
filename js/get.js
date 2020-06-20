1cf20c4297381e9efeb873820e5dc70f87644bbc
NekrodNik <NP17210@yandex.ru>
2020-06-20 19:30:11 +0600

    WEB version 0.1 add

--- js/get.js
+++ js/get.js
@@ -1,14 +1,15 @@
 async function update_message(){
-	let url = 'https://serverpocegram.pythonanywhere.com/get_messages?after='+lasttimestamp;
+	let url = 'http://serverpocegram.pythonanywhere.com/get_messages?after='+lasttimestamp;
 	var response = await fetch(url);
 	var messages = await response.json()
 	messages = messages["messages"]
 	var index;
 	for (index = 0; index < messages.length; ++index) {
 		var message = messages[index]
-		var dt = message["time"]
+		var datemessages = new Date(message["timestamp"]*1000)
+		var dt = datemessages.getHours()+":"+datemessages.getMinutes()+":"+datemessages.getSeconds()
 		viewtext = viewtext + "["+dt+" "+message["username"]+"]\n";
-		viewtext = viewtext+message["text"]+"\n"
+		viewtext = viewtext + message["text"]+"\n\n"
 		lasttimestamp=message["timestamp"]
 	document.getElementById("view").innerHTML=viewtext
 	}
---
+++ js/post.js
@@ -0,0 +1,17 @@
+
+async function send_message(){
+	var date = new Date();
+	var user={
+		"username": document.getElementById('username').value,
+		"password":document.getElementById('password').value, 
+		"text":document.getElementById('input').value,
+		"timestamp":date.getTime()
+	}
+	document.getElementById('input').value=""
+	let url = 'http://serverpocegram.pythonanywhere.com/send_message';
+	await fetch(url,{
+	method: 'POST',
+	headers: {'Content-Type': 'application/json;charset=utf-8'},
+	body: JSON.stringify(user)
+});
+}
